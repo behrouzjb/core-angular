871d85470a84d0cf75effc9a3a799b2fb8eb87a7
stagoapp <stagoapp@users.noreply.github.com>
2019-10-19 19:45:47 -0400

    User model created

---
+++ CoreAngular.API/CoreAngular.API/DAL/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreAngular.API.DAL.Models
+{
+    public class User : Entity
+    {
+        public int Id { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public string UserName { get; set; }
+        public string PublicId { get; set; }
+        public string Description { get; set; }
+        public string Interests { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string Country { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateLastActive { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public int CreatedBy { get; set; }
+        public int ModifiedBy { get; set; }
+    }
+}
