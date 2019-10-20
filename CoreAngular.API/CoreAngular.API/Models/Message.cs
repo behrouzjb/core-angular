397321d4b3e4c3496ef066e461dd17078747a915
stagoapp <stagoapp@users.noreply.github.com>
2019-10-20 02:44:47 -0400

    Models updated

---
+++ CoreAngular.API/CoreAngular.API/Models/Message.cs
@@ -0,0 +1,24 @@
+﻿using CoreAngular.API.DAL.Models;
+using System;
+using System.Collections.Generic;
+
+namespace CoreAngular.API.Models
+{
+    public partial class Message : Entity
+    {
+        public int Id { get; set; }
+        public int SendingUserId { get; set; }
+        public int ReceivingUserId { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public bool IsRead { get; set; }
+        public bool IsSenderDeleted { get; set; }
+        public bool IsReceiverDeleted { get; set; }
+        public DateTime? DateRead { get; set; }
+        public DateTime DateSent { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public int CreatedBy { get; set; }
+        public int ModifiedBy { get; set; }
+    }
+}
