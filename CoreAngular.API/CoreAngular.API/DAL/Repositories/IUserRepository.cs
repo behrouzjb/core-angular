3b2971338469db58f6cea52cdc2f33b7d53a862d
stagoapp <stagoapp@users.noreply.github.com>
2019-10-20 00:19:57 -0400

    Repository updated

---
+++ CoreAngular.API/CoreAngular.API/DAL/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using CoreAngular.API.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreAngular.API.DAL.Repositories
+{
+    public interface IUserRepository : IRepository<User>
+    {
+
+    }
+}
